bfeff5edf9648f1a4cd8191a1b53fec65171559d
alessandrodaniello105 <kanymsevement@gmail.com>
2023-11-29 15:57:36 +0100

    [add]: projects+techs+type route in admin sidebar

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -2,8 +2,9 @@
     <nav>
         <ul>
             <li><a href="{{route('admin.home')}}">Dashboard</a></li>
-            <li><a href="#">Lista xxx</a></li>
-            <li><a href="#">Lista yyy</a></li>
+            <li><a href="{{route('admin.projects.index')}}">Lista Progetti</a></li>
+            <li><a href="{{route('admin.technologies.index')}}">Lista Tecnologie</a></li>
+            <li><a href="{{route('admin.types.index')}}">Lista Tipi</a></li>
         </ul>
     </nav>
 </aside>

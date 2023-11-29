1cb70ce8f284c5b9c63002feb72a5ed2e86c31c3
alessandrodaniello105 <kanymsevement@gmail.com>
2023-11-29 16:19:26 +0100

    [edit]: now types are get by config file

--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.admin')
 
 @section('content')
+@php
+    $types = config('types')
+@endphp
+
 
     <h1>Lista Tipi</h1>
 
     <ul>
         @foreach ($types as $type)
         <li>
-            {{$type->name}}
+            {{$type}}
         </li>
         @endforeach
 

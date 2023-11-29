15f61c4ecf9bf2c4a9b94a65be40211f8f5e1036
alessandrodaniello105 <kanymsevement@gmail.com>
2023-11-29 15:34:10 +0100

    [chore] laravel 10.x + pacificdev + auth

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,9 @@
+<aside class="bg-dark">
+    <nav>
+        <ul>
+            <li><a href="{{route('admin.home')}}">Dashboard</a></li>
+            <li><a href="#">Lista xxx</a></li>
+            <li><a href="#">Lista yyy</a></li>
+        </ul>
+    </nav>
+</aside>
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- Font-Awesome 6.4.2 free --}}
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css' integrity='sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==' crossorigin='anonymous'/>
+
+    <title>Admin - Dashboard</title>
+    <!-- Usando Vite -->
+    @vite(['resources/js/app.js', 'resources/scss/app.scss'])
+</head>
+<body>
+
+    @include('admin.partials.header')
+
+    <div class="main-wrapper d-flex">
+        @include('admin.partials.sidebar')
+        @yield('content')
+    </div>
+
+</body>
+</html>

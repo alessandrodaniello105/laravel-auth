415440317ffb684a36e44672083f15ad52f5bf86
alessandrodaniello105 <kanymsevement@gmail.com>
2023-11-29 15:55:14 +0100

    [add]:Technology migr+seeder+model+controller+view

---
+++ resources/views/admin/technologies/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1>Lista Tecnologie</h1>
+
+    <ul>
+        @foreach ($technologies as $technology)
+        <li>
+            <a href="{{$technology->link}}">{{$technology->name}}</a>
+        </li>
+        @endforeach
+
+    </ul>
+
+@endsection
